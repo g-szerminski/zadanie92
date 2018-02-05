ffd19910940475318b55d1d9f712752763ffbf45
Grzegorz Szermiński <szerman@poczta.fm>
2018-02-05 21:55:45 +0100

    Fixed js code formating

--- scripts.js
+++ scripts.js
@@ -2,12 +2,10 @@ var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 var allNames = femaleNames.concat(maleNames);
 console.log(allNames);
-
 var newName = 'Marian';
 if (allNames.indexOf(newName) === -1) { 
-	var allNames = allNames.push(newName);
-    console.log('Dodane mię do tablicy to ' + newName);
-
-}	else {
+	allNames.push(newName);
+    console.log('Dodane imię do tablicy to ' + newName);
+} else {
 	console.log('Wprowadzone imię to ' + newName + ' Imię nie może być urzyte kolejny raz');
 }
