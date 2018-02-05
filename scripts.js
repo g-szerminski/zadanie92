aafae701d11441c7ee2e0922212c3838fcd21681
Grzegorz Szermiński <szerman@poczta.fm>
2018-02-05 22:03:33 +0100

    Fixed js code formating

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ console.log(allNames);
 var newName = 'Marian';
 if (allNames.indexOf(newName) === -1) { 
 	allNames.push(newName);
-    console.log('Dodane imię do tablicy to ' + newName);
+	console.log('Dodane imię do tablicy to ' + newName);
 } else {
 	console.log('Wprowadzone imię to ' + newName + ' Imię nie może być urzyte kolejny raz');
 }
